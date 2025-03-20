56eb096c0a216dd4809f31b79d7e85815b72c0c6
evavirseda <evirseda@boxfish.studio>
2025-03-20 16:05:46 +0100

    update ci

--- .github/scripts/check_sri_hash.sh
+++ .github/scripts/check_sri_hash.sh
@@ -31,10 +31,10 @@ done
 
 # If any file is outdated, exit with an error
 if [ "$OUTDATED" = true ]; then
-    echo -e "\n🚨🚨🚨"
-    echo "The CSS file hash at webassets.iota.org has changed."
+    echo -e "\n _________"
+    echo "⚠️ The CSS file hash at webassets.iota.org has changed."
     echo "Please update the affected files with the new hash: sha384-$NEW_HASH"
-    echo "🚨🚨🚨"
+    echo "_________"
     exit 1
 else
     echo "✅ All integrity hashes are up to date."
